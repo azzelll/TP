4df83e5112889affbf964bc50446535d9ec4e8d5
azzelll <madeshandykrisnanda@gmail.com>
2024-09-17 16:32:22 +0700

    update

--- 2406495615_MadeShandyKrisnanda.py
+++ 2406495615_MadeShandyKrisnanda.py
@@ -1,15 +1,20 @@
 import turtle as t
 import random as r
+
 t.pensize(10)
 t.title("Olympic Logo and Colorful Chessboard")
 t.speed(0)
 t.colormode(255)
+
+#memasukan warna sesuai dengan kode RGB ke dalam variable dan list
 biru = 0,129,200
 kuning = 252,177,49
 hitam = 0,0,0,
 merah = 0,166,81
 hijau = 238,51,78
-pen_color = [biru, kuning, hitam, merah, hijau ]
+pen_color = [biru, kuning, hitam, merah, hijau]
+
+#membuat fungsi untuk membuat logo olimpiade bergelomvbang serta membuat menjadi interlock
 def olympic_wave():
     t.penup()
     t.goto(-100,-50)
@@ -29,7 +34,7 @@ def olympic_wave():
         t.circle(50)
         t.penup()
         t.pendown()
-    for i in range(3):
+    for i in range(3): #membuat lingkaran interlock
         t.penup()
         t.pencolor(pen_color[i*2])
         t.home()
@@ -45,7 +50,7 @@ def olympic_wave():
         t.pendown()
         t.circle(50, 45)
         
-        
+#membuat fungsi untuk membuat garis olimpiade garis lurus
 def olympic_line():
     t.speed(0)
     t.penup()
@@ -61,7 +66,7 @@ def olympic_line():
     t.penup()
     t.goto(-100,-220)
     t.speed()
-    for i in range(4):
+    for i in range(4): #membuat lingkaran interlock
         t.pencolor(pen_color[i])
         t.penup()
         t.home()
@@ -82,8 +87,8 @@ def asking_box():
         t.forward(square_size*i)
         t.left(90)
         t.pendown()
-        for i in range(sum_rows):
-            if i > 0 :
+        for i in range(sum_rows): #menggambar kotak dan mengisinya dengan warna random
+            if i > 0 : 
                 t.forward(square_size)
             r_color = r.randint(0, 255)
             g_color = r.randint(0, 255)
@@ -95,7 +100,7 @@ def asking_box():
                 t.forward(square_size)
                 t.right(90)
             t.end_fill()
-def write_olympic():
+def write_olympic(): #menuliskan kata kata dibawah kotak
     t.penup()
     t.right(90)
     t.forward(square_size)
@@ -106,7 +111,7 @@ def write_olympic():
     t.pencolor("blue")
     t.write(f"Olympic Logo and Colorful Chessboard of {sum_rows**2} squares", move=False, align="center", font=("arial",15,"normal"))
 
-if __name__=="__main__":
+if __name__=="__main__": #progam utama
     t.screensize(2000,2000)
     sum_rows = int(t.numinput("Olympic Logo and Colorful Chessboard", "Enter the number of row:", minval=2, maxval=25,))
     square_size = int(t.numinput("Olympic Logo and Colorful Chessboard","Enter the squaresize(pixels):", minval=5, maxval=50))
